cfd61ffc59acb95fe3d5494c33c8440679034a27
PILLI DURGA PRASAD <durgaprasadpilli02@gmail.com>
2024-11-04 00:47:55 +0530

    update

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename="/durgaprasad-portfolio">
+  <BrowserRouter basename="/Durgaprasad-Portfolio">
     <App />
   </BrowserRouter>
 );
